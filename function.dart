53de05118bfc0dc83d1b8347cb95abd7b986be9c
nnivxix <adhe.hanasa@gmail.com>
2023-08-07 14:04:33 +0700

    feat: function return value

--- function.dart
+++ function.dart
@@ -6,9 +6,16 @@ void greet(String name) {
   print('Halo ${name}');
 }
 
+String calculate(int first, int second) {
+  return 'The result ${first} and ${second} is: ${first + second}';
+}
+
 void main() {
   String name = 'Nusa';
   sayHello();
   greet('Hanasa');
   greet(name);
+
+  final result = calculate(2, 3);
+  print(result);
 }
