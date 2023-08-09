60897b5553b2146fa479008f5dd4cb74e05c0c3b
nnivxix <adhe.hanasa@gmail.com>
2023-08-09 08:56:05 +0700

    feat: class_constructor

--- class.dart
+++ class.dart
@@ -3,17 +3,21 @@ class User {
   String password = '';
   String username = '';
 
+  User(this.email, this.password, this.username);
+
   String greet() {
     return 'halo ${this.username}';
+  }
+
+  String login() {
+    return 'You are login as ${this.username}';
   }
 }
 
 void main() {
-  var hanasa = User()
-    ..email = 'hanasa@hanasa.com'
-    ..password = '1234'
-    ..username = 'hanasa';
+  var hanasa = User('hanasa@hanasa.com', '1234', 'hanasa');
 
-  print(hanasa.username);
+  print(hanasa.email);
+  print(hanasa.login());
   print(hanasa.greet());
 }
