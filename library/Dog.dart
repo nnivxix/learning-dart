ce6e2e7bfd2944d8f255585629e6427b0107de0c
nnivxix <adhe.hanasa@gmail.com>
2023-08-13 19:51:08 +0700

    feat: inheritance comparation

--- library/Dog.dart
+++ library/Dog.dart
@@ -1,10 +1,17 @@
 import 'Animal.dart';
 
-class Dog extends Animal {}
+class Dog extends Animal {
+  String guk() {
+    return '${this.name} speak Guk guk 🐶';
+  }
+}
 
 void main() {
   var dogi = Dog();
   dogi.name = 'Dogie';
 
+  print(dogi is Dog);
+  print(dogi is Animal);
   print(dogi.move());
+  print(dogi.guk());
 }
